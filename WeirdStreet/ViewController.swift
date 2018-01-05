eddd266874d7e3d57cb0e3cf62214050405f13ef
Pablo Sedano <p.rivera@globant.com>
2018-01-04 20:00:11 -0600

    Initial Commit

---
+++ WeirdStreet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeirdStreet
+//
+//  Created by Pablo Sedano on 1/4/18.
+//  Copyright © 2018 Pablo Sedano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
